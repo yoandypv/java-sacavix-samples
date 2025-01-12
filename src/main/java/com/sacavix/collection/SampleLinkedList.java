c8b4d15de00b5b85cbfd924fe7c8d7290174b531
Yoandy Pérez Villazón <31771358+yoandypv@users.noreply.github.com>
2025-01-12 00:10:22 -0300

    Add sample of linkedlist

--- src/main/java/com/sacavix/collection/SampleLinkedList.java
+++ src/main/java/com/sacavix/collection/SampleLinkedList.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class SampleLinkedList {
 
-    public static void main(String[] args) {
+    public static void list() {
 
         // Add Item
         List<String> stringList = new LinkedList<>();
@@ -16,5 +16,48 @@ public class SampleLinkedList {
         stringList.forEach(System.out::println);
 
         stringList.clear();
+    }
+
+    public static void stack() {
+
+        LinkedList<String> stack = new LinkedList<>();
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+
+        System.out.println("Stack items: " + stack);
+
+        // Top example
+        String topElement = stack.pop();
+        System.out.println("Top item: " + topElement);
+        System.out.println("Stack items: " + stack);
+
+        // See the top element
+        System.out.println("Top item: " + stack.peek());
+    }
+
+
+    public static void queue() {
+        LinkedList<String> queue = new LinkedList<>();
+
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+
+        System.out.println("Queue items: " + queue);
+
+        // dequeue first item A
+        String firstElement = queue.poll();
+        System.out.println("First item: " + firstElement);
+        System.out.println("Queue items: " + queue);
+
+        // See first element
+        System.out.println("First item: " + queue.peek());
+    }
+
+
+    public static void main(String[] args) {
+       list();
     }
 }
